3714cc27d26e5c29eb71b8e92d659e50e41c6ba0
sergiocohens <sergiocohensalama@pursuit.org>
2020-02-27 00:52:55 -0500

    search operational

--- queries/searchQueries.js
+++ queries/searchQueries.js
@@ -2,18 +2,29 @@ const db = require("../helpers/db");
 
 const searchNew = async input => {
   const getQuery = `
-      SELECT *
-      FROM resourcers JOIN products ON resourcers.id = products.resourcers_id JOIN materials ON products.material_id = materials.id
-      WHERE materials.name ILIKE '%' || $1 || '%';
+      SELECT resourcers.id,
+             resourcers.company,
+             array_agg (materials.name) AS materials
+      FROM resourcers
+      JOIN products ON resourcers.id = products.resourcers_id
+      JOIN materials ON products.material_id = materials.id
+      WHERE materials.name ILIKE '%' || $1 || '%'
+      GROUP BY resourcers.id;
     `;
   return await db.any(getQuery, [input]);
 }
 
 const searchReclaimed = async input => {
   const getQuery = `
-      SELECT *
-      FROM creators JOIN reclaims ON creators.id = reclaims.creator_id
-      WHERE reclaims.composition ILIKE '%' || $1 || '%';
+      SELECT creators.id,
+             firstname,
+             lastname,
+             array_agg(distinct concat(reclaims.composition)) AS materials,
+             COUNT (reclaims.id)
+      FROM creators
+      JOIN reclaims ON creators.id = reclaims.creator_id
+      WHERE reclaims.composition ILIKE '%' || $1 || '%'
+      GROUP BY creators.id;
     `;
   return await db.any(getQuery, [input]);
 }
