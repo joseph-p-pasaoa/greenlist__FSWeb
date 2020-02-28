c5b46f30ad455d02a3c988d89a458351d4f0472b
sergiocohens <sergiocohensalama@pursuit.org>
2020-02-28 15:45:26 -0500

    search now return avatar_url and is ordered by amount of matches

--- queries/searchQueries.js
+++ queries/searchQueries.js
@@ -3,13 +3,17 @@ const db = require("../helpers/db");
 const searchNew = async input => {
   const getQuery = `
       SELECT resourcers.id,
+             resourcers.avatar_url,
              resourcers.company,
              array_agg (materials.name) AS materials
+             COUNT (materials.id)
       FROM resourcers
       JOIN products ON resourcers.id = products.resourcers_id
       JOIN materials ON products.material_id = materials.id
       WHERE materials.name ILIKE '%' || $1 || '%'
-      GROUP BY resourcers.id;
+      GROUP BY resourcers.id
+      ORDER BY COUNT (materials.id) DESC
+      ;
     `;
   return await db.any(getQuery, [input]);
 }
@@ -17,14 +21,17 @@ const searchNew = async input => {
 const searchReclaimed = async input => {
   const getQuery = `
       SELECT creators.id,
+             avatar_url,
              firstname,
              lastname,
-             array_agg(distinct concat(reclaims.composition)) AS materials,
+             array_agg( distinct reclaims.composition ORDER BY reclaims.composition ASC ) AS materials,
              COUNT (reclaims.id)
       FROM creators
       JOIN reclaims ON creators.id = reclaims.creator_id
       WHERE reclaims.composition ILIKE '%' || $1 || '%'
-      GROUP BY creators.id;
+      GROUP BY creators.id
+      ORDER BY COUNT (reclaims.id) DESC
+      ;
     `;
   return await db.any(getQuery, [input]);
 }
